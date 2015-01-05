872b866104b01d27b8a6ef39b3d548f3d01d7fcb
Belthazod@gmail.com <Belthazod@gmail.com@1d4de6f7-4903-4ab4-21ff-f80b8391a82a>
2015-01-05 03:39:06 +0000

    Uploaded 1st version of IT Project prototype

---
+++ ItProjectII/src/itprojectii/ItProjectII.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itprojectii;
+
+/**
+ *
+ * @author weak_2030
+ */
+public class ItProjectII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.setVisible(true);
+    }
+    
+}
