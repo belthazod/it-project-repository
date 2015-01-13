598e94146f21d967f54233c5a6302688377e17a7
Belthazod@gmail.com <Belthazod@gmail.com@1d4de6f7-4903-4ab4-21ff-f80b8391a82a>
2015-01-13 06:42:04 +0000

    Something

--- ItProjectII/src/itprojectii/ItProjectII.java
+++ ItProjectII/src/itprojectii/ItProjectII.java
@@ -15,7 +15,12 @@ public class ItProjectII {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        UserLogIn main = new UserLogIn();
+        /*UserLogIn main = new UserLogIn();
+        main.setVisible(true);
+        main.setTitle("Just Another Inventory System");
+        */
+        
+        Main main = new Main();
         main.setVisible(true);
         main.setTitle("Just Another Inventory System");
     }
