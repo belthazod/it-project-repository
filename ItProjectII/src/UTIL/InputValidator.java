d49bc041dc0d8bd8c6cccbffcc439d62576efc42
Belthazod@gmail.com <Belthazod@gmail.com@1d4de6f7-4903-4ab4-21ff-f80b8391a82a>
2015-02-22 07:18:00 +0000

    Moved and Updated DatabaseConnector and InputValidator to another package [UTIL]

---
+++ ItProjectII/src/UTIL/InputValidator.java
@@ -0,0 +1,44 @@
+
+package UTIL;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/**
+ * The InputValidator servers as a tool check user Input depending on the 
+ * conditions set.
+ * @author Belthazod
+ */
+public class InputValidator {
+    
+    public static boolean checkInput(String input, String message){
+        boolean result = true;
+        if(input.equals("")){
+            JOptionPane.showMessageDialog(null, message, "Input error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        return result;
+    }
+    
+    public static void clearInput(JTextField[] inputs){
+        for(int i = 0; i<inputs.length ; i++){
+            inputs[i].setText("");
+        }
+    }
+    
+    public static void clearInput(JTextField input){
+        input.setText("");
+    }
+    
+    public static boolean checkIfNumber(String input, String message){
+        try{
+            Integer.parseInt(input);
+            return true;
+        }catch(NumberFormatException x){
+            x.printStackTrace();
+            
+            JOptionPane.showMessageDialog(null, message, "Input error", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+    }
+}
