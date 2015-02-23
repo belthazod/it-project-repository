a8243e40f7174d871ee3f3d659220b20d57d5434
macadaegharvey@gmail.com <macadaegharvey@gmail.com@1d4de6f7-4903-4ab4-21ff-f80b8391a82a>
2015-02-23 13:26:59 +0000

    Updated reports

--- ItProjectII/src/itprojectii/Reports.java
+++ ItProjectII/src/itprojectii/Reports.java
@@ -30,15 +30,18 @@ public class Reports extends javax.swing.JPanel {
         jComboBox1 = new javax.swing.JComboBox();
         jComboBox2 = new javax.swing.JComboBox();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jLabel3 = new javax.swing.JLabel();
-        jComboBox3 = new javax.swing.JComboBox();
         jComboBox5 = new javax.swing.JComboBox();
         jComboBox6 = new javax.swing.JComboBox();
+        jLabel4 = new javax.swing.JLabel();
+        jComboBox4 = new javax.swing.JComboBox();
+        jComboBox7 = new javax.swing.JComboBox();
+        jComboBox8 = new javax.swing.JComboBox();
+        jComboBox9 = new javax.swing.JComboBox();
 
         setMaximumSize(new java.awt.Dimension(814, 592));
         setPreferredSize(new java.awt.Dimension(814, 592));
@@ -62,9 +65,6 @@ public class Reports extends javax.swing.JPanel {
             }
         });
         add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(845, 491, -1, -1));
-
-        jButton2.setText("Back To Main");
-        add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(744, 491, -1, -1));
 
         jLabel1.setText("Category:");
         add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 48, -1, -1));
@@ -101,13 +101,10 @@ public class Reports extends javax.swing.JPanel {
         ));
         jScrollPane1.setViewportView(jTable1);
 
-        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 71, 910, 330));
+        add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 70, 930, 330));
 
-        jLabel3.setText("Date :");
-        add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 40, -1, -1));
-
-        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" }));
-        add(jComboBox3, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 40, -1, -1));
+        jLabel3.setText("To :");
+        add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 20, -1, -1));
 
         jComboBox5.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" }));
         jComboBox5.addActionListener(new java.awt.event.ActionListener() {
@@ -115,10 +112,30 @@ public class Reports extends javax.swing.JPanel {
                 jComboBox5ActionPerformed(evt);
             }
         });
-        add(jComboBox5, new org.netbeans.lib.awtextra.AbsoluteConstraints(810, 40, -1, -1));
+        add(jComboBox5, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 40, -1, -1));
 
         jComboBox6.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016" }));
-        add(jComboBox6, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 40, -1, -1));
+        add(jComboBox6, new org.netbeans.lib.awtextra.AbsoluteConstraints(870, 40, -1, -1));
+
+        jLabel4.setText("From :");
+        add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 20, -1, 10));
+
+        jComboBox4.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" }));
+        add(jComboBox4, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 40, -1, -1));
+
+        jComboBox7.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" }));
+        add(jComboBox7, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 40, -1, -1));
+
+        jComboBox8.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" }));
+        jComboBox8.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox8ActionPerformed(evt);
+            }
+        });
+        add(jComboBox8, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 40, -1, -1));
+
+        jComboBox9.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016" }));
+        add(jComboBox9, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 40, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
@@ -133,18 +150,25 @@ public class Reports extends javax.swing.JPanel {
         // TODO add your handling code here:
     }//GEN-LAST:event_jComboBox5ActionPerformed
 
+    private void jComboBox8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox8ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox8ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JComboBox jComboBox1;
     private javax.swing.JComboBox jComboBox2;
-    private javax.swing.JComboBox jComboBox3;
+    private javax.swing.JComboBox jComboBox4;
     private javax.swing.JComboBox jComboBox5;
     private javax.swing.JComboBox jComboBox6;
+    private javax.swing.JComboBox jComboBox7;
+    private javax.swing.JComboBox jComboBox8;
+    private javax.swing.JComboBox jComboBox9;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
