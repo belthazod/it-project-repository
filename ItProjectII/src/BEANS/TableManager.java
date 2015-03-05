64c4315fcf8ad665b0f0441b5e276d8d67ac1ec6
Belthazod@gmail.com <Belthazod@gmail.com@1d4de6f7-4903-4ab4-21ff-f80b8391a82a>
2015-03-05 03:28:21 +0000

    Added Documentation comments to <Table Manager.java>

--- ItProjectII/src/BEANS/TableManager.java
+++ ItProjectII/src/BEANS/TableManager.java
@@ -18,6 +18,13 @@ import javax.swing.table.DefaultTableModel;
 public class TableManager {
     private  DefaultTableModel model;
     private JTable table;
+    
+ /**
+ *  Creates and instance of TableManager linking it to a particular table which
+ *  will later on be modified through the TableManager methods.
+ *  @param table - the selected table to be modified and controlled.
+ *
+ */
     public TableManager(JTable table) {
         try{
             this.table = table;
@@ -28,12 +35,27 @@ public class TableManager {
         }
     }
     
+ /**
+ *  Clears the table of all rows leaving it empty. Column headers are retained.
+ *
+ */
     public void clearTableContents(){
         for(int row = 0; row < model.getRowCount();){
             model.removeRow(0);
         }   
     }
     
+ /**
+ *  Clears the JTable of any current rows and values and afterwards adds 
+ *  rows and values to the JTable based on the result of a query.
+ *  The number of columns in the JTable should match the number of attributes
+ *  in the Result Set.
+ *  
+ *  @param rs - the database Result Set produced from a query
+ *  @exception SQLException - The database Result Set cannot be found or the
+ *      header count of the ResultSet does not match the column count of
+ *      the JTable. 
+ */ 
     public void importDBContents(ResultSet rs) throws SQLException{
         clearTableContents();
         for(int row = 0; rs.next(); row++){
@@ -43,22 +65,50 @@ public class TableManager {
             }
         }
     }
-    
+
+ /**
+ *  Deletes the row currently shown as selected in the JTable.
+ *
+ */
     public void deleteRow(){
         model.removeRow(table.getSelectedRow());
     }
-    
+
+ /**
+ *  Adds a set of values in the form of a row to the JTable
+ *
+ *  @param values - a String array containing all the values to be added as a 
+ *      row to the JTable.
+ */
     public void addRowContent(String[] values){
         model.addRow(new Vector());
-        for(int col = 0; col<model.getColumnCount(); col++){
+        for(int col = 0; col<values.length; col++){
                 model.setValueAt(values[col], model.getRowCount()-1, col);
             }
     }
     
+ /**
+ *  Returns the row index of the highlighted or selected row in the JTable. If 
+ *  multiple rows are selected, the returned value is always the result of the 
+ *  first selected row.
+ * 
+ *  @return selectedRow - the index of the single selected row in the JTable
+ * 
+ */
     public int getSelectedRow(){
         return table.getSelectedRow();
     }
-    public String getIDFromTable(int row){
+    
+  /**
+ *  Returns the ID of a particular row in the JTable containing values from the 
+ *  database. This is based on the row number
+ * 
+ *  @param row - the row index of the JTable containing the desired element ID.
+ *  @return id - the id mapped to the database.
+ *  @exception NullPointerException - throws this when the specified row cannot 
+ *      be found from the JTable.
+ */
+    public String getIDFromTable(int row) throws NullPointerException{
         return table.getValueAt(row, 0).toString();
     }
 }
