11de60a90ef993d757b4073f4bf24dd602c2572a
Belthazod@gmail.com <Belthazod@gmail.com@1d4de6f7-4903-4ab4-21ff-f80b8391a82a>
2015-03-12 03:26:40 +0000

    Added UserModule(incomplete)

---
+++ ItProjectII/src/CONTROLLERS/UserController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CONTROLLERS;
+
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Belthazod
+ */
+public class UserController {
+    
+    public UserController(JTable userTable, JButton openEditUserButton, JButton deleteUserButton, JTextField usernameInput,JTextField nameInput, JPasswordField passwordInput, JButton addUserButton){
+        
+    }
+    
+    public void addUser(){
+        
+    }
+}
